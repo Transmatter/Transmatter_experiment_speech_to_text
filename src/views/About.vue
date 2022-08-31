686c1da9a6f09d2f101665c66d831bedc49dee23
oat431 <oat431@gmail.com>
2022-08-31 22:09:02 +0700

    [💚 update] : ci script

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,41 @@
 <template>
-    <div class="flex justify-center">
-        <h1 class="my-6 text-3xl font-bold">
-            About
-        </h1>
+    <div class="lg:mx-80 lg:my-6 lg:px-3 border">
+        <div class="flex justify-center">
+            <h1 class="my-6 text-3xl font-bold">Transmatter instruction</h1>
+        </div>
+        <div class="divider"></div>
+        <p class="my-2 hover:bg-green-50">1. โปรดใช้ตัวอักษร <kbd class="kbd">X</kbd> บนแป้นพิมพ์ของท่านเพื่อเปลี่ยนหมวดหมู่ของข่าวต่างๆ</p>
+        <div class="divider"></div>
+        <p class="my-2 hover:bg-green-50">
+            2.
+            หากท่านต้องการหาข่าวสารแบบเฉพาะเจาะจง โปรดกด
+            <kbd class="kbd">space bar</kbd> ทางเว็บไซต์จะทำการเปิดโหมดค้นหาข้อมูล ท่านสามารถใส่ keyword ได้ทันที
+            หลังจากนั้นโปรดกด <kbd class="kbd">enter</kbd>
+        </p>
+        <div class="divider"></div>
+        <p class="my-2 hover:bg-green-50">
+            3.
+            หลังจากที่ท่านได้ทำการค้นหาแล้ว เว็บไซต์จะทำการตรวจทานการค้นหาของท่าน หาก keyword ที่ท่านใส่มา มีการสะกดผิด
+            ทางเว็บไซต์จะแนะนำคำที่ควรจะเป็นให้แก่ท่าน
+        </p>
+        <div class="divider"></div>
+        <p class="my-2 hover:bg-green-50"> 4. โดยจะมีทั้งหมด 3 คำ โดย 2 คำแรกเป็นคำที่ทางเว็บไซ์ได้เสนอและอีกคำคือคำของท่านเอง</p>
+        <div class="divider"></div>
+        <p class="my-2 hover:bg-green-50">
+            5. 
+            โดยท่านสามารถกด <kbd class="kbd">Z</kbd>, <kbd class="kbd">X</kbd> หรือ
+            <kbd class="kbd">C</kbd> เพื่อเลือกคำที่ท่านต้องการตามลำดับ
+        </p>
+        <div class="divider"></div>
+        <p class="my-2 hover:bg-green-50">6. และหากท่านต้องการย้อนกลับหน้าเดิม กด <kbd class="kbd">◀︎</kbd></p>
+        <div class="divider"></div>
+        <p class="my-2 hover:bg-green-50">7. หากต้องการทำตรงข้าม กด <kbd class="kbd">▶︎</kbd></p>
+        <div class="divider"></div>
+        <p class="my-2 hover:bg-green-50">
+            8.
+            หรือถ้าท่านอยู่ในหน้า home กด <kbd class="kbd">▼</kbd> เพื่อข่าวเพิ่มเติม และกด
+            <kbd class="kbd">▲</kbd> เพื่อไปหน้า Home
+        </p>
+        <div class="divider"></div>
     </div>
 </template>
