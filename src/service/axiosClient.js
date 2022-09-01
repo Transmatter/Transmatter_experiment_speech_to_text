afc0805e11a4ca541da8143cb47f35e2e16f00af
oat431 <oat431@gmail.com>
2022-09-01 23:04:52 +0700

    [🐛 fixed] : audio port

--- src/service/axiosClient.js
+++ src/service/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://52.230.104.188:8080",
+  baseURL: "http://52.230.104.188:3917",
 
   headers: {
     Accept: "application/json",
